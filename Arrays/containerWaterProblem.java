35dfd0c0fa7fdd0360f2af84b74f829846fc0d63
Atharva Jamdade <168163867+atharvaj1425@users.noreply.github.com>
2025-07-08 00:34:38 +0530

    fifth commit

---
+++ Arrays/containerWaterProblem.java
@@ -0,0 +1,27 @@
+public class containerWaterProblem {
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, width * minHeight);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println("Maximum water that can be contained: " + maxArea(height));
+    }
+}
