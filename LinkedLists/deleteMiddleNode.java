98ddb65fd2bc65331bba138597217f3f4b17145a
Atharva Jamdade <168163867+atharvaj1425@users.noreply.github.com>
2025-07-15 00:45:09 +0530

    eleventh commit

---
+++ LinkedLists/deleteMiddleNode.java
@@ -0,0 +1,23 @@
+public class deleteMiddleNode {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow=head, fast=head, temp=head;
+        if(head==null || head.next==null){
+            return null;
+        }
+        fast=fast.next.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
